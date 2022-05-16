31605983636b981500924f5ad9cc19541e073f98
Kamppix <42889487+Kamppix@users.noreply.github.com>
2022-05-16 09:04:05 +0300

    Initial commit

---
+++ src/main/java/com/github/kamppix/environmentalmusic/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package com.github.kamppix.environmentalmusic.mixin;
+
+import com.github.kamppix.environmentalmusic.EnvironmentalMusic;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		EnvironmentalMusic.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
